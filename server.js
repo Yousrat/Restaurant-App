e217d4e54dda7454b3a5fc3daae1ebda877ca0fb
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-15 13:00:14 -0700

    Single array for customer list.

--- server.js
+++ server.js
@@ -22,36 +22,49 @@ app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-var tables = [{
+var reserved = [{
     customerName:"rima",
     phoneNumber:"234567871",
     customerEmail:"rima@mail.com",
-    customerID:"1234"
+    customerID:"0001"
   },{
     customerName:"jemin",
     phoneNumber:"994567871",
     customerEmail:"jemin@mail.com",
-    customerID:"9234"
-  }];
-
-var waitlist = [{
+    customerID:"0002"
+  },{
     customerName:"sam",
     phoneNumber:"8767871",
     customerEmail:"sam@mail.com",
-    customerID:"1234"
+    customerID:"0003"
   },{
     customerName:"amala",
     phoneNumber:"8854994567871",
     customerEmail:"amala@mail.com",
-    customerID:"9234"
+    customerID:"0004"
+  },{
+    customerName:"geetha",
+    phoneNumber:"678967871",
+    customerEmail:"geetha@mail.com",
+    customerID:"0005"
+  },{
+    customerName:"alex",
+    phoneNumber:"834567871",
+    customerEmail:"alex@mail.com",
+    customerID:"0006"
+  },{
+    customerName:"basil",
+    phoneNumber:"994567871",
+    customerEmail:"basil@mail.com",
+    customerID:"0007"
   }];
 
 app.get('/api/tables', function (req, res) {
-  res.json(tables);
+  res.json(reserved.slice(0, 5));
 });
 
 app.get('/api/waitlist', function (req, res) {
-  res.json(waitlist);
+  res.json(reserved.slice(5));
 });
 
 app.listen(PORT, function() {
