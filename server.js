f7386ade88ec22f5419386bc934ce1e6cae5a489
Erin <eringlabe@gmail.com>
2017-04-15 12:00:06 -0700

    tables route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
